3cda65a806bd953f181470c87795867de4cbc0b3
Daniel Cloud <daniel@danielcloud.org>
2015-09-12 00:10:07 -0400

    Set up an OS X Swift app skeleton.

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TalkingBook
+//
+//  Created by Daniel Cloud on 9/11/15.
+//  Copyright © 2015 Daniel Cloud. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TalkingBook
+//
+//  Created by Daniel Cloud on 9/11/15.
+//  Copyright © 2015 Daniel Cloud. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
