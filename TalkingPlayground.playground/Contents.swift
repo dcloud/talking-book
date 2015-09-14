bd08d548008c92927bd4d68c9bd36c144e19f528
Daniel Cloud <daniel@danielcloud.org>
2015-09-14 12:04:58 -0400

    Save playground.

--- TalkingPlayground.playground/Contents.swift
+++ TalkingPlayground.playground/Contents.swift
@@ -2,6 +2,28 @@
 
 import Cocoa
 
+/*
+let dtdStr = "smil PUBLIC \"-//W3C//DTD SMIL 1.0//EN\" \"http://www.w3.org/TR/REC-smil/SMIL10.dtd\""
 
 
+let dtdNode = NSXMLDTDNode(XMLString: dtdStr)
 
+let doctypeString = "<!DOCTYPE \(dtdStr)>"
+let dtdNode1 = NSXMLDTDNode(XMLString: doctypeString)
+
+let dtdCopied = "<!DOCTYPE smil PUBLIC \"-//W3C//DTD SMIL 1.0//EN\" \"http://www.w3.org/TR/REC-smil/SMIL10.dtd\">"
+let dtdNodeCopied = NSXMLDTDNode(XMLString: dtdCopied)
+
+let xhtmlNode = try? NSXMLDTD(contentsOfURL: NSURL(string: "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd")!, options: NSXMLNodePreserveEntities)
+
+print(xhtmlNode?.description)
+*/
+
+public extension String {
+    func format(args: CVarArgType...) -> String {
+        return NSString(format: self, arguments: getVaList(args)) as String
+    }
+}
+
+
+print("%04.1f".format(1.3))
