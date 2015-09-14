9a201b03679c5d0a779af0857508e6e32e239285
Daniel Cloud <daniel@danielcloud.org>
2015-09-14 11:41:49 -0400

    Start some tests for SMIL XML.

--- TalkingBookTests/XMLTests.swift
+++ TalkingBookTests/XMLTests.swift
@@ -41,7 +41,22 @@ class XMLTests: XCTestCase {
         XCTAssertGreaterThan(root.children.count, 0)
         XCTAssertNotNil(body.parent)
         XCTAssertNotNil(rootChild)
-        XCTAssertNotNil(rootChild.parent)
+        XCTAssertNotNil(rootChild!.parent)
+    }
+    
+    func testXMLDocumentInit() {
+        let root = XMLElement("root")
+        let doc = XMLDocument(root, version: 1.0)
+        
+        XCTAssertNotNil(doc.root)
+        XCTAssertEqual(root.name, "root")
+    }
+    
+    func testXMLDocumentConvenienceInit() {
+        let doc = XMLDocument("root", version: 1.0)
+        
+        XCTAssertNotNil(doc.root)
+        XCTAssertEqual(doc.root.name, "root")
     }
     
     func testXMLToString() {
@@ -83,4 +98,79 @@ class XMLTests: XCTestCase {
         }
     }
 
+}
+
+
+class SMILXMLTests: XCTestCase {
+    let smilDTDv1 = "smil PUBLIC \"-//W3C//DTD SMIL 1.0//EN\" \"http://www.w3.org/TR/REC-smil/SMIL10.dtd\""
+    var smilDTD: DTD?
+    var smilDoc: XMLDocument?
+    
+    override func setUp() {
+        super.setUp()
+        self.smilDTD = DTD(self.smilDTDv1)
+        self.smilDoc = XMLDocument("smil", version: 1.0, docType: smilDTDv1)
+    }
+    
+    override func tearDown() {
+        self.smilDoc = nil
+        self.smilDTD = nil
+        super.tearDown()
+    }
+    
+    func testSMILDTD() {
+        if let smilDoc = self.smilDoc, smilDTD = self.smilDTD {
+            if let docDTD = smilDoc.dtd {
+                XCTAssertEqual(docDTD.toString(), smilDTD.toString())
+            } else {
+                XCTFail("smilDoc.dtd was not properly initialized")
+            }
+            
+        } else {
+            XCTFail("smilDoc was not properly initialized!")
+        }
+    }
+    
+    func testSMILDocument() {
+        let head = XMLElement("head")
+        
+        let metasByAttrs: [AttributeDictionary] = [
+            ["name": "dc:format", "content": "Daisy 2.02"],
+            ["name": "ncc:generator", "content": "XMLTests"],
+            ["name": "dc:identifier", "content": "abcdef-123-abc"]
+        ]
+        
+        for attrs:AttributeDictionary in metasByAttrs {
+            let meta = XMLElement("meta", attributes: attrs, selfClosing: true)
+            head.addChild(meta)
+        }
+        
+        for child in head.children {
+            XCTAssertEqual(child.name, "meta")
+        }
+        
+        
+        let body = XMLElement("body")
+        let mainSeq = XMLElement("seq", attributes: ["dur": "1.3s"])
+        body.addChild(mainSeq)
+        
+        XCTAssertEqual(body.children.count, 1)
+        XCTAssertEqual(body.children[0].name, "seq")
+        if let dur: String = body.children[0].attributes["dur"] as? String {
+            XCTAssertEqual(dur, "1.3s")
+        }
+        
+        if let smilDoc = self.smilDoc {
+            smilDoc.addChild(head)
+            smilDoc.addChild(body)
+            
+            XCTAssertNotEqual(smilDoc.toString(), "")
+            
+
+//            let xmlDocument: NSXMLDocument? = try? NSXMLDocument(XMLString: smilDoc.toString(), options: Int(NSXMLDocumentContentKind.XMLKind.rawValue))
+            
+//            XCTAssertNotNil(xmlDocument)
+        }
+        
+    }
 }
